c31a3c00489b8ab397b01603032a19dec280b694
Rashmi Bhavsar <94361221+rash2615@users.noreply.github.com>
2023-05-18 23:04:04 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// Connexion à la base de données PostgreSQL
+$dsn = 'pgsql:host=postgres;dbname=todo_db';
+$user = 'postgres';
+$password = 'votre_mot_de_passe';
+$db = new PDO($dsn, $user, $password);
+
+// Exécutez une requête pour récupérer les données de la table "todo"
+$query = $db->query('SELECT * FROM todo');
+$todos = $query->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Todo App</title>
+</head>
+<body>
+    <h1>Todo List</h1>
+    <ul>
+        <?php foreach ($todos as $todo): ?>
+            <li><?php echo $todo['id']; ?> - <?php echo $todo['titre']; ?> (<?php echo $todo['done'] ? 'Fait' : 'À faire'; ?>)</li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
